7e14f571c4ec253f576ff07141b3e4edc9083845
pramodtng <psingay96@gmail.com>
2022-11-23 11:38:03 +0600

    changes

--- pages/gallery.js
+++ pages/gallery.js
@@ -6,11 +6,10 @@ import Image from 'next/image'
 const https = require('https');
 
 const gallery = ({ res }) => {
-  console.log(JSON.stringify(res))
   const STRAPI_BASEURL = 'https://tashielectronicsbackend.tashicell.com'
 
   return (
-    <div class="container mx-auto space-y-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-3 px-10">
+    <div className="container mx-auto space-y-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-3 px-10">
       {
         res.data.map(function (image) {
           return (
@@ -30,10 +29,12 @@ export async function getStaticProps() {
   const agent = new https.Agent({
     rejectUnauthorized: false
   });
-  const datas = await fetch('https://tashielectronicsbackend.tashicell.com/api/galleries?populate=*',{
+  const datas = await fetch('https://tashielectronicsbackend.tashicell.com/api/galleries?populate=*'
+  ,{
     method: 'GET',
     agent
-  })
+  }
+  )
   const res = await datas.json();
   return {
     props: {
